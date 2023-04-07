a5caf6ff74c30a290ef77a6d2679c70637b21313
Tanya <tt.laurie@gmail.com>
2023-04-07 21:13:02 +0300

    creation console app about films

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
+const numberOfFilms = +prompt('Сколько фильмов вы смотрели?', '');
 
-let inc = 10;
-let dec = 10;
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
 
+const lastWachedFilm1 = prompt('Один из последних просмотренных фильмов', '');
+const lastWachedFilmRate1 = prompt('На сколько оцениваете его?', '');
+const lastWachedFilm2 = prompt('Один из последних просмотренных фильмов', '');
+const lastWachedFilmRate2 = prompt('На сколько оцениваете его?', '');
 
+personalMovieDB.movies[lastWachedFilm1] = lastWachedFilmRate1;
+personalMovieDB.movies[lastWachedFilm2] = lastWachedFilmRate2;
 
+console.log(personalMovieDB);
